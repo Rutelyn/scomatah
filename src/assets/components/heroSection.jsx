861e7ec2f9dd86e7c684b66f901c1f61379486e1
Harika <ruth.agbozo.93@gmail.com>
2025-02-07 16:25:56 +0000

    hero section

--- src/assets/components/heroSection.jsx
+++ src/assets/components/heroSection.jsx
@@ -1,8 +1,16 @@
-
+import bgYellow from "../images/bgYellow.png";
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div>
+     <div>
+<img src= {bgYellow}/>
+<h2>Start Your Healthy Snacking Journey With Us</h2>
+     </div>
+     <div>
+
+     </div>
+    </div>
   )
 }
 
