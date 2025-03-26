d08fda75a9e332c6ec752ce8ec95791bc391fb2b
Harika <ruth.agbozo.93@gmail.com>
2025-03-26 18:32:31 +0000

    lib index

--- src/assets/lib/readSheet/index.jsx
+++ src/assets/lib/readSheet/index.jsx
@@ -55,68 +55,7 @@ export const getSheetsData = async () => {
             summary: data[start][1],
         });
     }
-    
-     extractData = (data) => {
-        const aboutUs = { title: data[1][3], summary: [2][4] }
 
-        let abtStart, abtEnd
-        let abt = []
-
-        let i = 0
-        while (true) {
-            if (data[i][0] === "END") break; 
-
-            if (data[i][0] === "About Start") {
-                abtStart = i+3;
-            }
-            if (data[i][0] === "About End") {
-                abtEnd = i-2;
-            }
-
-        }
-
-
-    let start, end;
-    start = abtStart
-    end = abtEnd
-    for (;start <= end; start++) {
-        abt.push({
-            title: data[start][0],
-            summary: data[start][1],
-        });
-    }
-
-    }
-
-     extractData = (data) => {
-        const products = { title: data[1][3] }
-
-        let proStart, proEnd
-        let pro = []
-
-        let i = 0
-        while (true) {
-            if (data[i][0] === "END") break; 
-
-            if (data[i][0] === "Product Start") {
-                abtStart = i+5;
-            }
-            if (data[i][0] === "Product End") {
-                abtEnd = i-1;
-            }
-
-        }
-
-
-    let start, end;
-    start = proStart
-    end = proEnd
-    for (;start <= end; start++) {
-        abt.push({
-            title: data[start][0],
-        });
-    }
-    }
     
 }
 }
